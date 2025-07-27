4022b0c64074842ef9a65caa9f2921fea577ba4c
Danilo Torquato <d.torquato.dev@gmail.com>
2025-07-27 10:40:51 -0300

    Configura e conecta ao gitHub

---
+++ src/test/java/dev/DanilloTorquato/AlocacaoDeImoveis/AlocacaoDeImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.DanilloTorquato.AlocacaoDeImoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlocacaoDeImoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
